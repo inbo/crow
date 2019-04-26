dc39158614de87e5e20763bae1ef69f363bb9e30
Peter Desmet <peter.desmet.work@gmail.com>
2019-04-26 09:41:52 +0200

    Move drawLineChart to plotVpi module

--- src/main.js
+++ src/main.js
@@ -61,61 +61,6 @@ function calculateMtr(data, altMin = 0, altMax = Infinity,
     return mtr
 }
 
-function drawLineChart(data) {
-    // cfr. https://medium.freecodecamp.org/learn-to-create-a-line-chart-using-d3-js-4f43f1ee716b
-    var svgWidth = 1000, svgHeight = 400;
-    var margin = { top: 20, right: 20, bottom: 30, left: 50 };
-    var width = svgWidth - margin.left - margin.right;
-    var height = svgHeight - margin.top - margin.bottom;
-
-    var svg = d3.select('svg')
-        .attr("width", svgWidth)
-        .attr("height", svgHeight)
-
-    var g = svg.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var xScale = d3.scaleTime()
-        .domain(d3.extent(data, d =>  d.datetime ))
-        .rangeRound([0, width]);
-
-    var yScale = d3.scaleLinear()
-        .domain(d3.extent(data, d =>  d.mtr))
-        .rangeRound([height, 0]);
-
-    var line = d3.line()
-        .defined(d => !isNaN(d.mtr))
-        .x(d => xScale(d.datetime))
-        .y(d => yScale(d.mtr));
-
-    g.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(xScale))
-        .select(".domain")
-        .remove();
-
-    g.append("g")
-        .call(d3.axisLeft(yScale))
-        .append("text")
-        .attr("fill", "#000")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "0.71em")
-        .attr("text-anchor", "end")
-        .text("Number of individuals");
-
-    g.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round")
-        .attr("stroke-width", 1.5)
-        .attr("d", line);
-
-
-}
-
 // Read example data file
 const file = "./data/example_vpts_20160901.csv"
 const dataset = readVpts(file);
@@ -149,7 +94,7 @@ csv_parsed.then(function(data) {
         })); // 400, 2000, 200, 3
 
     console.log(mtr_profile);
-    drawLineChart(mtr_profile);
+    plotVpi(mtr_profile);
 });
 
 
--- src/modules/plotVpi.js
+++ src/modules/plotVpi.js
@@ -1,5 +1,55 @@
 function plotVpi(vpiData, height = 300, width = 600, el = "#plotVpi") {
-    return "Called from plotVpi";
+    const data = vpiData;
+    // cfr. https://medium.freecodecamp.org/learn-to-create-a-line-chart-using-d3-js-4f43f1ee716b
+    var svgWidth = 1000, svgHeight = 400;
+    var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    var width = svgWidth - margin.left - margin.right;
+    var height = svgHeight - margin.top - margin.bottom;
+
+    var svg = d3.select('svg')
+        .attr("width", svgWidth)
+        .attr("height", svgHeight)
+
+    var g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var xScale = d3.scaleTime()
+        .domain(d3.extent(data, d =>  d.datetime ))
+        .rangeRound([0, width]);
+
+    var yScale = d3.scaleLinear()
+        .domain(d3.extent(data, d =>  d.mtr))
+        .rangeRound([height, 0]);
+
+    var line = d3.line()
+        .defined(d => !isNaN(d.mtr))
+        .x(d => xScale(d.datetime))
+        .y(d => yScale(d.mtr));
+
+    g.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xScale))
+        .select(".domain")
+        .remove();
+
+    g.append("g")
+        .call(d3.axisLeft(yScale))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Number of individuals");
+
+    g.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);
 }
 
 export { plotVpi }
