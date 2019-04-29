aae4c897e664611599919604c2b42f2e83762094
Peter Desmet <peter.desmet.work@gmail.com>
2019-04-29 08:48:04 +0200

    Refactor reusable chart
    Based on https://www.toptal.com/d3-js/towards-reusable-d3-js-charts
    
    Add test case with two small datasets

--- src/modules/plotVpi.js
+++ src/modules/plotVpi.js
@@ -1,84 +1,125 @@
 function plotVpi() {
   console.log("plotVpi() called");
 
-  let svgWidth = 600;
-  let svgHeight = 300;
+  // Options accessible to caller
+  let width = 600;
+  let height = 300;
+  let data = [];
+  let updateWidth;
+  let updateHeight;
+  let updateData;
 
   function chart(selection) {
-    console.log("chart() called");
+    selection.each(function () {
+      // Define chart dimensions
+      let margin = { top: 20, right: 20, bottom: 30, left: 50 };
+      let chartWidth = width - margin.left - margin.right;
+      let chartHeight = height - margin.top - margin.bottom;
+      let transitionDuration = 750;
 
-    // Get data
-    let data = selection.datum();
+      // Create svg and chart element
+      let dom = d3.select(this);
+      let svg = dom.append("svg")
+        .attr("class", "vpi-plot")
+        .attr('height', height)
+        .attr('width', width);
+      let canvas = svg.append("g")
+        .attr("class", "canvas")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // Set margin, height and width
-    let margin = { top: 20, right: 20, bottom: 30, left: 50 };
-    let width = svgWidth - margin.left - margin.right;
-    let height = svgHeight - margin.top - margin.bottom;
-    
-    // Select or create svg element
-    let svg = selection
-      .selectAll("svg")
-      .data([1])
-      .enter()
-      .append("svg");
-    svg
-      .attr("width", svgWidth)
-      .attr("height", svgHeight);
+      // Define ranges
+      let xRange = d3.scaleTime().rangeRound([0, chartWidth]);
+      let yRange = d3.scaleLinear().rangeRound([chartHeight, 0]);
 
-    let g = svg.append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
-    let xScale = d3.scaleTime()
-      .domain(d3.extent(data, d =>  d.datetime ))
-      .rangeRound([0, width]);
+      // Define axis
+      var xAxis = d3.axisBottom(xRange);
+      var yAxis = d3.axisLeft(yRange);
 
-    let yScale = d3.scaleLinear()
-      .domain(d3.extent(data, d =>  d.mtr))
-      .rangeRound([height, 0]);
+      // Items below depend on data
 
-    let line = d3.line()
-      .defined(d => !isNaN(d.mtr))
-      .x(d => xScale(d.datetime))
-      .y(d => yScale(d.mtr));
+      // Set value line
+      let valueLine = d3.line()
+        .defined(d => !isNaN(d.mtr))
+        .x(d => xRange(d.datetime))
+        .y(d => yRange(d.mtr));
 
-    g.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(xScale))
-      .select(".domain")
-      .remove();
+      // Scale the range of the data
+      xRange.domain(d3.extent(data, d => d.datetime ));
+      yRange.domain(d3.extent(data, d => d.mtr ));
 
-    g.append("g")
-      .call(d3.axisLeft(yScale))
-      .append("text")
-      .attr("fill", "#000")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("Number of individuals");
+      // Add x axis
+      canvas.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0," + chartHeight + ")")
+        .call(xAxis)
+        .select(".domain")
+        .remove();
 
-    g.append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-      .attr("stroke-width", 1.5)
-      .attr("d", line);
+      // Add y axis
+      canvas.append("g")
+        .attr("class", "y-axis")
+        .call(yAxis)
+        .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Number of individuals");
+      
+      // Add value line
+      canvas.append("path")
+        .attr("class", "line")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", valueLine);
+
+      // Update functions
+      updateData = function() {
+        // Scale the range of the data again
+        xRange.domain(d3.extent(data, d => d.datetime ));
+        yRange.domain(d3.extent(data, d => d.mtr ));
+
+        // Make changes
+        svg.select(".line")
+          .transition()
+          .duration(transitionDuration)
+          .attr("d", valueLine(data))
+        svg.select(".x-axis")
+          .transition()
+          .duration(transitionDuration)
+          .call(xAxis)
+        svg.select(".y-axis")
+          .transition()
+          .duration(transitionDuration)
+          .call(yAxis)
+      }
+    });
   }
 
-  // Get/set width of svg
   chart.width = function(value) {
-		if (!arguments.length) return svgWidth;
-    svgWidth = value;
-		return chart;
+    if (!arguments.length) return width;
+    width = value;
+    // if (typeof updateWidth === 'function') updateWidth(); // updateWidth() not implemented
+    return chart;
   };
-  
-  // Get/set height of svg
+
   chart.height = function(value) {
-		if (!arguments.length) return svgHeight;
-    svgHeight = value;
-		return chart;
+    if (!arguments.length) return height;
+    height = value;
+    // if (typeof updateHeight === 'function') updateHeight(); // updateHeight() not implemented
+    return chart;
+  };
+
+  chart.data = function(value) {
+    if (!arguments.length) return data;
+    data = value;
+    if (typeof updateData === 'function') updateData();
+    return chart;
   };
   
   return chart;
