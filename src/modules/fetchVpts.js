b1026bcf6242362ca4e10e58b1a9937a048a2acf
stijnvanhoey <stijnvanhoey@gmail.com>
2019-04-30 10:52:40 +0200

    Draft implementation fetching

--- src/modules/fetchVpts.js
+++ src/modules/fetchVpts.js
@@ -1,5 +1,23 @@
 function fetchVpts(dateMin = null, dateMax = null, directory = ".") {
-    return "Called from fetchVptsData";
+    /*
+    We assume files have a certain frequency, eg day and the date is available
+    in the file name (e.g. example_vpts_20160901.csv)
+
+    startDate - endDate
+        => retrieve the different required files
+        => compose the URLS
+    */
+    if (startDate > endDate) {throw "StartDate need to be before endDate"}
+    // define each date within range with given resolution (e.g. day/month)
+    let dates = []
+    while (startDate < endDate) {
+        dates.push(new Date(startDate));
+        startDate.setMonth(startDate.getDay() + 1);
+    }
+    console.log(dates);
+
+    // compose string array of dates
+    //dates.map(date => date.)
 }
 
 async function readVpts(file) {
@@ -8,3 +26,4 @@ async function readVpts(file) {
 }
 
 export { fetchVpts, readVpts }
+
