ebd88fb1246f93e9c1bed9cf836f1bfaa7ba4bbc
stijnvanhoey <stijnvanhoey@gmail.com>
2019-05-06 17:50:02 +0200

    Add datebased filegeneration

--- src/modules/fetchVpts.js
+++ src/modules/fetchVpts.js
@@ -1,24 +1,41 @@
-function fetchVpts(dateMin = null, dateMax = null, directory = ".") {
+function fetchVpts(dateMin = null, dateMax = null, directory = "./") {
     /*
-    We assume files have a certain frequency, eg day and the date is available
+    We assume files have daily frequency and the date is available
     in the file name (e.g. example_vpts_20160901.csv)
 
     startDate - endDate
-        => retrieve the different required files
-        => compose the URLS
+        => retrieve the different required dates
+        => compose the filenames
     */
-    if (startDate > endDate) {throw "StartDate need to be before endDate"}
-    // define each date within range with given resolution (e.g. day/month)
-    let dates = []
-    while (startDate < endDate) {
-        dates.push(new Date(startDate));
-        startDate.setMonth(startDate.getDay() + 1);
-    }
-    console.log(dates);
 
-    // compose string array of dates
-    //dates.map(date => date.)
+    // parse the datestrings
+    dateMin = new Date(dateMin);
+    dateMax = new Date(dateMax);
+
+    if (dateMin > dateMax) {
+      throw "dateMin need to be before dateMax"
+    };
+
+    // collect each day of the dates in between min and max date
+    let dates = [];
+    let filenames = [];
+    dates.push(new Date(dateMin));
+    while (dateMin < dateMax) {
+        console.log(dateMin);
+        dateMin.setDate(dateMin.getDate() + 1);
+        dates.push(new Date(dateMin));
+    }
+
+    // compose filename string array with dates
+    let pre_string = "example_vpts_"
+    let post_string = ".csv"
+    dates = dates.map(date => date.toISOString().split("T")[0].replace(/\D/g,''))
+    filenames = dates.map(date => directory + pre_string + date + post_string)
+
+    return filenames
 }
+
+console.log(fetchVpts("2016-09-01", "2016-09-03", "../public/data/"))
 
 async function readVpts(file) {
   let response = await d3.csv(file, d => {
