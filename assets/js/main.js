574f787a50a473e6f3dd0539d878fcbf3ce70590
Peter Desmet <peter.desmet.work@gmail.com>
2019-08-02 13:14:11 +0200

    Don't display example dataset + timeout

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,64 +7,19 @@ import { integrateProfile } from "./modules/integrateProfile.js";
 import { plotVpts } from "./modules/plotVpts.js";
 import { plotVpi } from "./modules/plotVpi.js";
 
-// Read example data files
-const dataset1 = fetchVpts("2016-09-01", "2016-09-01", "data/");
-console.log(dataset1);
-const dataset2 = fetchVpts("2016-09-02", "2016-09-03", "data/");
-console.log(dataset2);
-
-// Define and bind (empty) vpts chart
-let vptsChart = plotVpts().width(800).height(300);
-d3.select("#vpts-chart")
-  .call(vptsChart);
-
-// Define and bind (empty) vpi chart
+// Define and bind (empty) charts
 let vpiChart = plotVpi().height(300);
 d3.select("#vpi-chart")
   .call(vpiChart);
 
-// Load first dataset
-dataset1.then(function(data) {
-  let nestedData = d3.nest()
-    .key(d => d.datetime) // group data by datetime
-    .entries(data);
-  
-  // Create mtr profile
-  let mtrProfile = nestedData.map(d => {
-    return {
-      datetime: d.key,
-      mtr: integrateProfile(d.values)
-    }
-  });
+let vptsChart = plotVpts().width(800).height(300);
+d3.select("#vpts-chart")
+  .call(vptsChart); // TODO: use this one
 
-  // Create subset of vpts data
-  let subset = data.slice(0, 5);
-
-  // Bind to vpts chart
-  // vptsChart.data(subset);
-  
-  // Bind to vpi chart
-  vpiChart.data(mtrProfile);
-});
-
-// Wait 1 second and load second dataset
-// TODO: don't repeat parsing functionality
-setTimeout(function() {
-    dataset2.then(function(data) {
-      let nestedData = d3.nest()
-        .key(d => d.datetime) // group data by datetime
-        .entries(data);
-      let mtrProfile = nestedData.map(d => {
-        return {
-          datetime: d.key,
-          mtr: integrateProfile(d.values)
-        }
-      });
-      vpiChart.data(mtrProfile);
-    });
-  },
-  1000
-);
+// Load example dataset
+// TODO: remove this test
+const exampleDataset = fetchVpts("2016-09-01", "2016-09-03", "data");
+console.log(exampleDataset);
 
 // --- Vue.js ---
 
