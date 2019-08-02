2101cee2ef9dc9c9b3c18a9c3b7a435f38fd883f
Peter Desmet <peter.desmet.work@gmail.com>
2019-08-02 13:07:50 +0200

    Parse fixed-width kmi data in readVpts

--- assets/js/modules/fetchVpts.js
+++ assets/js/modules/fetchVpts.js
@@ -43,18 +43,46 @@ async function fetchVpts(startDate = new Date(), endDate = new Date(), path = ""
     }, []);
 }
 
-async function readVpts(file) {
-  let response = await d3.csv(file, d => {
-    return {
-      datetime: Date.parse(d.datetime), // TODO: use new Date() to get actual dates rather than milliseconds since January 1, 1970
-      height: +d.height,
-      dd: +d.dd,
-      ff: +d.ff,
-      dens: +d.dens,
-      sd_vvp: d.sd_vvp
-    };
-  });
-  return response;
+async function readVpts(file, format = "bioRad") {
+  let data = [];
+
+  if (format == "kmi") {
+    // kmi vpts are formatted as fixed width data
+    let rawData = await d3.text(file)
+    data = d3.csvParseRows(rawData, d => {
+      let row = d[0];
+      return {
+        datetime: Date.parse(
+          row.substring(0, 4) + "-" + row.substring(4, 6) + "-" + row.substring(6, 8) + 
+          "T" + row.substring(9, 11) + ":" + row.substring(11, 13)
+        ),
+        height: parseInt(row.substring(14, 18)),
+        dd: parseFloat(row.substring(47, 52)),
+        ff: parseFloat(row.substring(41, 46)),
+        dens: parseFloat(row.substring(76, 82)),
+        sd_vvp: parseFloat(row.substring(53, 59))
+      };
+    });
+    data = data.slice(4); // Remove 4 header rows
+  
+  } else if (format == "bioRad") {
+    // bioRad vpts are formatted as csv
+    data = await d3.csv(file, d => {
+      return {
+        datetime: Date.parse(d.datetime), // TODO: use new Date() to get actual dates rather than milliseconds since January 1, 1970
+        height: +d.height,
+        dd: +d.dd,
+        ff: +d.ff,
+        dens: +d.dens,
+        sd_vvp: d.sd_vvp
+      };
+    });
+  
+  } else {
+    throw "Format " + format + " not recognized."
+  }
+  
+  return data;
 }
 
 export { fetchVpts, readVpts }
