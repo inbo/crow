febe3c7439216ea079b8c82736deb5fb8122da97
Nicolas Noé <nicolas@niconoe.eu>
2019-12-06 14:36:14 +0100

    Vtps data is now properly loaded/parsed.

--- crow-vue-prototype/src/components/Crow.vue
+++ crow-vue-prototype/src/components/Crow.vue
@@ -1,19 +1,16 @@
 <template>
   <div>
-    <b-alert :show="dataLoadError" variant="danger">
-        Data can't be loaded!
-    </b-alert>
-
+    <b-alert :show="dataLoadError" variant="danger">Data can't be loaded!</b-alert>
 
     <b-form-row>
       <b-col class="mt-4">
         <b-form inline @submit.prevent="loadData">
-          
           <b-form-group>
-          <b-form-select v-model="selectedRadar" :options='availableRadars'></b-form-select>
-          <small class="form-text text-muted" id="radar-help">
-            {{ selectedRadarName }} is located in {{ selectedRadarCountry }}
-          </small>
+            <b-form-select v-model="selectedRadar" :options="availableRadars"></b-form-select>
+            <small
+              class="form-text text-muted"
+              id="radar-help"
+            >{{ selectedRadarName }} is located in {{ selectedRadarCountry }}</small>
           </b-form-group>
 
           <!-- TODO: Add other fields? -->
@@ -34,7 +31,7 @@
 import moment from "moment";
 import axios from "axios";
 
-import config from "../config"
+import config from "../config";
 
 export default {
   data() {
@@ -45,20 +42,53 @@ export default {
 
       dataLoadError: false,
 
-      radarData: ''
+      radarVtps: "",
+      evt: null // Store event, for debug purposes
     };
   },
   methods: {
+    // TODO: move following method to a "helper" JS module
+    // TODO: also move its constants (number of header lines, fiels position, ...) to config.js
+    readVtps(responseString) {
+      let d = responseString.split("\n");
+
+      d = d.map(function(row) {
+        return {
+          datetime: Date.parse(
+            row.substring(0, 4) +
+              "-" +
+              row.substring(4, 6) +
+              "-" +
+              row.substring(6, 8) +
+              "T" +
+              row.substring(9, 11) +
+              ":" +
+              row.substring(11, 13)
+          ),
+          height: parseInt(row.substring(14, 18)),
+          dd: parseFloat(row.substring(47, 52)),
+          ff: parseFloat(row.substring(41, 46)),
+          dens: parseFloat(row.substring(76, 82)),
+          sd_vvp: parseFloat(row.substring(53, 59))
+        };
+      });
+
+      return d.splice(4); // Remove 4 header lines
+    },
     loadData() {
       let vm = this;
       let url = this.buildDataUrl(this.selectedRadar, this.selectedDate);
 
-      axios.get(url).then(response => {
-        vm.dataLoadError = false;
-        vm.radarData = response.data;
-      }).catch(function () {
-        vm.dataLoadError = true;
-      })
+      axios
+        .get(url)
+        .then(response => {
+          vm.dataLoadError = false;
+          vm.radarVtps = this.readVtps(response.data);
+        })
+        .catch(function(evt) {
+          vm.evt = evt;
+          vm.dataLoadError = true;
+        });
     },
     buildDataUrl(radarName, selectedDate) {
       let theDate = moment(selectedDate, config.localizedDateFormat);
@@ -74,8 +104,9 @@ export default {
       return this.availableRadars.find(d => d.value == this.selectedRadar).text;
     },
     selectedRadarCountry() {
-      return this.availableRadars.find(d => d.value == this.selectedRadar).country;
-    },
+      return this.availableRadars.find(d => d.value == this.selectedRadar)
+        .country;
+    }
   }
 };
 </script>
