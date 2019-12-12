d7d1ccb39ad968a902739bc47484676af4e581b4
Nicolas Noé <nicolas@niconoe.eu>
2019-12-12 16:19:47 +0100

    Graph now support a date range (slow and messy)

--- src/components/Crow.vue
+++ src/components/Crow.vue
@@ -11,16 +11,11 @@
               class="form-text text-muted"
               id="radar-help"
             >{{ selectedRadarName }} is located in {{ selectedRadarCountry }}</small>
-          </b-form-group>
-
+          </b-form-group>From:
           <!-- TODO: Add other fields? -->
-          <b-form-input
-            type="date"
-            placeholder="type a date..."
-            v-model="selectedDate"
-            class="mr-2"
-          />
-          <b-button type="submit">Submit</b-button>
+          <b-form-input type="date" placeholder="type a date..." v-model="startDate" class="mr-2" />To:
+          <b-form-input type="date" placeholder="type a date..." v-model="endDate" class="mr-2" />
+          <b-button type="submit">Load radar data</b-button>
         </b-form>
       </b-col>
     </b-form-row>
@@ -36,14 +31,23 @@ import VtpsChart from "./VtpsChart.vue";
 
 import moment from "moment";
 import axios from "axios";
+import _ from "lodash";
 
 import config from "../config";
 import helpers from "../helpers";
 
+// TODO: Use moment objects everywhere (currently date in vtpsData, and string for v-model link)
+// TODO: HAve a "no data" color
+// Look at typescript?
+// Look at other fancy JS stuff available
+// Read let, var, const
+// TODO: validation of date min <= max
+
 export default {
   data() {
     return {
-      selectedDate: moment().format("YYYY-MM-DD"),
+      startDate: moment().format("YYYY-MM-DD"),
+      endDate: moment().format("YYYY-MM-DD"),
       selectedRadar: config.initialRadarCode,
       availableRadars: config.availableRadars,
 
@@ -54,27 +58,91 @@ export default {
     };
   },
   methods: {
-    loadData() {
-      let vm = this;
-      let url = this.buildDataUrl(this.selectedRadar, this.selectedDate);
+    emptyVtpsData(startDate, endDate) {
+      var start = moment(startDate, "YYYY-MM-DD")
+        .hour(0)
+        .minute(0)
+        .second(0);
+      var end = moment(endDate, "YYYY-MM-DD")
+        .hour(23)
+        .minute(59)
+        .second(59);
 
-      axios
-        .get(url)
-        .then(response => {
-          vm.dataLoadError = false;
-          vm.radarVtps = helpers.readVtps(response.data);
-        })
-        .catch(function(evt) {
-          vm.evt = evt;
-          vm.dataLoadError = true;
+      var currentMoment = start.clone();
+      var allHeights = _.range(0, 4800 + 1, 200);
+      var vm = this;
+
+      vm.radarVtps = [];
+
+      while (currentMoment.isBefore(end)) {
+        allHeights.forEach(function(height) {
+          vm.radarVtps.push({
+            datetime: currentMoment.toDate().getTime(),
+            height: height,
+            dd: 0,
+            ff: 0,
+            dens: 0,
+            sd_vvp: 0
+          });
         });
+
+        currentMoment.add(5, "minutes");
+      }
+    },
+
+    loadData() {
+      this.emptyVtpsData(this.startDate, this.endDate);
+      this.populateDataFromCrowServer(
+        this.selectedRadar,
+        this.startDate,
+        this.endDate
+      );
+    },
+    populateDataFromCrowServer(radarName, startDate, endDate) {
+      var startDay = moment(startDate, "YYYY-MM-DD");
+      var endDay = moment(endDate, "YYYY-MM-DD").add(1, "days");
+
+      var currentDay = startDay.clone();
+
+      var firstRun = true;
+
+      while (currentDay.isBefore(endDay, "day")) {
+        let url = this.buildDataUrl(radarName, currentDay);
+        var dayData = [];
+
+        axios
+          .get(url)
+          .then(response => {
+            dayData = helpers.readVtps(response.data);
+
+            // Merge new data into existing
+            dayData.forEach(element => {
+              // Find row matching time/altitude
+              var pos = this.radarVtps.findIndex(function(emptyElem) {
+                if (firstRun) {
+                  firstRun = false;
+                }
+
+                if (
+                  emptyElem.datetime === element.datetime &&
+                  emptyElem.height === element.height
+                ) {
+                  return true;
+                }
+              });
+
+              this.radarVtps.splice(pos, 1, element); // Call splice so reactivity works
+            });
+          })
+          .catch(function() {});
+
+        currentDay.add(1, "days");
+      }
     },
     buildDataUrl(radarName, selectedDate) {
-      let theDate = moment(selectedDate, config.localizedDateFormat);
-
-      return `${config.dataBaseUrl}/${radarName}/${theDate.format(
+      return `${config.dataBaseUrl}/${radarName}/${selectedDate.format(
         "YYYY"
-      )}/${radarName}_vpts_${theDate.format("YYYYMMDD")}.txt`;
+      )}/${radarName}_vpts_${selectedDate.format("YYYYMMDD")}.txt`;
     }
   },
 
@@ -89,7 +157,7 @@ export default {
   },
 
   components: {
-    VtpsChart,
+    VtpsChart
   }
 };
 </script>
