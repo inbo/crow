e08dd79067a7dc451bcdbd3e75fd0c4019491dc8
Nicolas Noé <nicolas@niconoe.eu>
2019-12-13 10:31:16 +0100

    Perf improvements with computed properties.

--- src/components/VtpsChart.vue
+++ src/components/VtpsChart.vue
@@ -24,32 +24,34 @@ export default {
       this.renderChart(val);
     }
   },
-  methods: {
-    getRectDivider() {
-      var durationInMs = this.getMaxDatetime() - this.getMinDatetime();
+  computed: {
+    rectDivider: function() {
+      var durationInMs = this.maxDatetime - this.minDatetime;
       return durationInMs / 1000 / 60 / this.dataTemporalResolution;
     },
-    getMinDatetime() {
+    minDatetime: function() {
       return this.vtpsData.reduce(
         (min, p) => (p.datetime < min ? p.datetime : min),
         this.vtpsData[0].datetime
       );
     },
-    getMaxDatetime() {
+    maxDatetime: function() {
       return this.vtpsData.reduce(
         (max, p) => (p.datetime > max ? p.datetime : max),
         this.vtpsData[0].datetime
       );
     },
-    getMaxDensity() {
+    maxDensity: function() {
       return this.vtpsData.reduce(
         (max, p) => (p.dens > max ? p.dens : max),
         this.vtpsData[0].dens
       );
     },
-    getDistinctHeights() {  
+    distinctHeights: function() {
       return [...new Set(this.vtpsData.map(row => row.height))];
-    },
+    }
+  },
+  methods: {
     renderChart(vtpsData_val) {
       var margin = { top: 30, right: 30, bottom: 30, left: 30 },
         width = 1100 - margin.left - margin.right,
@@ -66,7 +68,7 @@ export default {
 
       var x = d3
         .scaleTime()
-        .domain([this.getMinDatetime(), this.getMaxDatetime()])
+        .domain([this.minDatetime, this.maxDatetime])
         .range([0, width]);
       svg
         .append("g")
@@ -76,16 +78,14 @@ export default {
       var y = d3
         .scaleBand()
         .range([height, 0])
-        .domain(this.getDistinctHeights())
+        .domain(this.distinctHeights)
       svg.append("g").call(d3.axisLeft(y));
-
-      this.deb = this.getDistinctHeights();
 
       // Build color scale
       var myColor = d3
         .scaleLinear()
         .range(["#69b3a2", "red"])
-        .domain([0, this.getMaxDensity()]);
+        .domain([0, this.maxDensity]);
 
       svg
         .selectAll()
@@ -98,7 +98,7 @@ export default {
         .attr("y", function(row) {
           return y(row.height);
         })
-        .attr("width", width / this.getRectDivider())
+        .attr("width", width / this.rectDivider)
         .attr("height", y.bandwidth())
         .style("fill", function(row) {
           if(row.noData) {
