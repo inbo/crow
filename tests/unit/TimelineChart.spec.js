b6b132a0a858216c1b2ec51b5ca97df0fb152256
Nicolas Noé <nicolas.noe@inbo.be>
2020-02-06 13:07:38 +0100

    More tests for TimelineChart

--- tests/unit/TimelineChart.spec.js
+++ tests/unit/TimelineChart.spec.js
@@ -3,44 +3,43 @@ import TimelineChart from '../../src/components/TimelineChart'
 import moment from 'moment-timezone';
 
 let styleConfig = {
-    margin: { top: 0, right: 60, bottom: 5, left: 65 },
-    width: 1277, // 1152 forn the graph, after we remove margins
-    height: 50,
+  margin: { top: 0, right: 60, bottom: 5, left: 65 },
+  width: 1277, // 1152 forn the graph, after we remove margins
+  height: 50,
 
-    timeAxisFormat: " D-M@HH:mm z",
+  timeAxisFormat: " D-M@HH:mm z",
 
-    showXAxis: true,
-    showTooltip: true,
+  showXAxis: true,
+  showTooltip: true,
 
-    dayColor: '#dae9fe',
-    twilightColor: '#4771bb',
-    nightColor: '#1e252d'
+  dayColor: '#dae9fe',
+  twilightColor: '#4771bb',
+  nightColor: '#1e252d'
 }
 
-
 let testPeriods = [
-    {"moment": moment("2020-01-23T10:30:00.000Z"), "sunAltitude": 19.200964203532266},
-    {"moment": moment("2020-01-23T10:35:00.000Z"), "sunAltitude": 19.26350235809867},
-    {"moment": moment("2020-01-23T10:40:00.000Z"), "sunAltitude": 19.309003099210862},
-    {"moment": moment("2020-01-23T10:45:00.000Z"), "sunAltitude": 19.33743073598932},
-    {"moment": moment("2020-01-23T10:50:00.000Z"), "sunAltitude": 19.348762956779822},
-    {"moment": moment("2020-01-23T10:55:00.000Z"), "sunAltitude": 19.342990938899007},
-    {"moment": moment("2020-01-23T11:00:00.000Z"), "sunAltitude": 19.32011936805931},
-    {"moment": moment("2020-01-23T11:05:00.000Z"), "sunAltitude": 19.280166422415686},
-    {"moment": moment("2020-01-23T11:10:00.000Z"), "sunAltitude": 19.223163700614474},
-    {"moment": moment("2020-01-23T11:15:00.000Z"), "sunAltitude": 19.149156143705536},
-    {"moment": moment("2020-01-23T11:20:00.000Z"), "sunAltitude": 19.05820181861885},
-    {"moment": moment("2020-01-23T11:25:00.000Z"), "sunAltitude": 18.950371795084237},
+  { "moment": moment("2020-01-23T10:30:00.000Z"), "sunAltitude": 19.200964203532266 },
+  { "moment": moment("2020-01-23T10:35:00.000Z"), "sunAltitude": 19.26350235809867 },
+  { "moment": moment("2020-01-23T10:40:00.000Z"), "sunAltitude": 19.309003099210862 },
+  { "moment": moment("2020-01-23T10:45:00.000Z"), "sunAltitude": 19.33743073598932 },
+  { "moment": moment("2020-01-23T10:50:00.000Z"), "sunAltitude": 19.348762956779822 },
+  { "moment": moment("2020-01-23T10:55:00.000Z"), "sunAltitude": 19.342990938899007 },
+  { "moment": moment("2020-01-23T11:00:00.000Z"), "sunAltitude": 19.32011936805931 },
+  { "moment": moment("2020-01-23T11:05:00.000Z"), "sunAltitude": 19.280166422415686 },
+  { "moment": moment("2020-01-23T11:10:00.000Z"), "sunAltitude": 19.223163700614474 },
+  { "moment": moment("2020-01-23T11:15:00.000Z"), "sunAltitude": 19.149156143705536 },
+  { "moment": moment("2020-01-23T11:20:00.000Z"), "sunAltitude": 19.05820181861885 },
+  { "moment": moment("2020-01-23T11:25:00.000Z"), "sunAltitude": 18.950371795084237 },
 ]
 
 test('General TimelineChart component rendering & behaviour', () => {
   const wrapper = mount(TimelineChart, {
     propsData: {
-        styleConfig: styleConfig,
-        dataTemporalResolution: 300,
-        showTimeAs: "Europe/Brussels",
-        periods: testPeriods
-      }
+      styleConfig: styleConfig,
+      dataTemporalResolution: 300,
+      showTimeAs: "Europe/Brussels",
+      periods: testPeriods
+    }
   });
 
   expect(wrapper.contains('svg')).toBe(true);
@@ -57,31 +56,75 @@ test('General TimelineChart component rendering & behaviour', () => {
   const lastTickText = wrapper.findAll('g.tick text').at(11)
   expect(lastTickText.text()).toBe("23-1@12:25 CET")
 
-  // Wit this basic data, everything should be 'day'
+  // With this basic dataset, everything should be 'day'
+  wrapper.findAll('rect').wrappers.forEach(wp => expect(wp.classes()).toContain('day'));
 
   // Check it can correctly autodetect the temporal resolution (5 minutes)
   expect(wrapper.vm.dataTemporalResolution).toBe(5 * 60);
-
-  //console.log(wrapper.html({ prettyPrint: true}));
 });
 
-// TODO: test popover can be enabled/disabled
+test("Have popovers when asked in the config", () => {
+    const wrapper = mount(TimelineChart, {
+      propsData: {
+        styleConfig: styleConfig,
+        dataTemporalResolution: 300,
+        showTimeAs: "Europe/Brussels",
+        periods: testPeriods
+      }
+    });
+
+    expect(wrapper.contains("b-popover")).toBe(true);
+    expect(wrapper.findAll('b-popover').length).toBe(12); // 1 per period
+    // TODO: check popover content
+});
+
+test("Don't have popovers when asked in the config", () => {
+  let disabledPopoverconfig = styleConfig;
+  disabledPopoverconfig.showTooltip = false;
+  
+  const wrapper = mount(TimelineChart, {
+    propsData: {
+      styleConfig: disabledPopoverconfig,
+      dataTemporalResolution: 300,
+      showTimeAs: "Europe/Brussels",
+      periods: testPeriods
+    }
+  });
+
+  expect(wrapper.contains("b-popover")).toBe(false);
+});
+
+
 // TOTO: check popover content
-// TODO: test axis can be disabled
 // TODO: should we split in multiple "test" blocks
 
 test('TimelineChart axis texts in UTC mode', () => {
   const wrapper = mount(TimelineChart, {
     propsData: {
-        styleConfig: styleConfig,
-        dataTemporalResolution: 300,
-        showTimeAs: "UTC",
-        periods: testPeriods
-      }
+      styleConfig: styleConfig,
+      dataTemporalResolution: 300,
+      showTimeAs: "UTC",
+      periods: testPeriods
+    }
   });
 
   // Data in UTC, display too. So the ticks should match the passed data, and have the 'UTC' mention
   expect(wrapper.find('g.tick text').text()).toBe("23-1@10:30 UTC");
   const lastTickText = wrapper.findAll('g.tick text').at(11)
   expect(lastTickText.text()).toBe("23-1@11:25 UTC")
+});
+
+test('TimelineChart axis can be disabled', () => {
+  let disabledAxisconfig = styleConfig;
+  disabledAxisconfig.showXAxis = false;
+
+  const wrapper = mount(TimelineChart, {
+    propsData: {
+      styleConfig: disabledAxisconfig,
+      dataTemporalResolution: 300,
+      showTimeAs: "UTC",
+      periods: testPeriods
+    }
+  });
+  expect(wrapper.contains('g.tick')).toBe(false)
 });
