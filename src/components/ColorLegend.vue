ba0c71bb09a137616b9bc543f8eb088a0a122ad9
Nicolas Noé <nicolas.noe@inbo.be>
2020-05-13 11:41:59 +0200

    Improved color scales, updated dependencies

--- src/components/ColorLegend.vue
+++ src/components/ColorLegend.vue
@@ -15,10 +15,10 @@
       style="position: absolute; left: 0px; top: 0px;"
     >
       <g
-        v-axis="{ 'scale': legendScaleSequential }"
+        v-axis="{ 'scale': axisScale }"
         :transform="`translate(${styleDiv.margin.left - 1}, ${styleDiv.margin.top + canvasHeight - 1})`"
         style="stroke-width: 0.5px"
-      /> 
+      />
     </svg>
   </div>
 </template>
@@ -27,15 +27,15 @@
 // Implementation heavily inspired by http://bl.ocks.org/syntagmatic/e8ccca52559796be775553b467593a9fx
 import Vue from "vue";
 import * as d3 from "d3";
- 
+
 export default Vue.extend({
   name: "ColorLegend",
   directives: {
     axis(el, binding): void {
       const scaleFunction = binding.value.scale;
-      const legendAxis = d3
-        .axisBottom<number>(scaleFunction)
+      const legendAxis = d3.axisBottom<number>(scaleFunction);
       legendAxis(d3.select((el as unknown) as SVGGElement));
+      //console.log(scaleFunction.thresholds())
     }
   },
   props: ["colorScale", "colorScaleType", "opacity", "topic"], // eslint-disable-line
@@ -49,12 +49,27 @@ export default Vue.extend({
     };
   },
   computed: {
+    axisScale: function():
+      | d3.ScaleLinear<number, number>
+      | d3.ScaleBand<string> {
+      let sc;
+
+      switch (this.colorScaleType) {
+        case "sequential":
+          sc = this.legendScaleSequential;
+          break;
+        default:
+          // The axis cannot be build directly from a scaleQuantize, so we build a scaleBand instead
+          sc = d3.scaleBand().domain(['0', '1', '2', '3', '4', '5', '6', '7','8', '9']).range(this.legendScaleOrdinal.domain()) // FIXME: domain should not be hardcoded, but read from colorScale doamin....
+      }
+      return sc;
+    },
     legendScaleSequential: function(): d3.ScaleLinear<number, number> {
       return d3
         .scaleLinear()
         .domain(this.colorScale.domain())
         .range([
-          1,  
+          1,
           this.styleDiv.width -
             this.styleDiv.margin.left -
             this.styleDiv.margin.right
@@ -64,7 +79,7 @@ export default Vue.extend({
       return d3
         .scaleQuantize()
         .domain([
-          1,  
+          1,
           this.styleDiv.width -
             this.styleDiv.margin.left -
             this.styleDiv.margin.right
@@ -111,19 +126,19 @@ export default Vue.extend({
       handler: function(newOpacity): void {
         this.clearCanvas();
         this.renderColorRamp(newOpacity);
-      },
+      }
     },
     colorScale: {
       handler: function(): void {
         this.clearCanvas();
         this.renderColorRamp(this.opacity);
-      },
+      }
     },
     colorScaleType: {
       handler: function(): void {
         this.clearCanvas();
         this.renderColorRamp(this.opacity);
-      },
+      }
     }
   },
   mounted: function() {
@@ -139,14 +154,13 @@ export default Vue.extend({
     },
     addOpacityToColor: function(colorStr: string, opacity: number): string {
       const colorObj = d3.color(colorStr);
-      
+
       if (colorObj != null) {
         colorObj.opacity = opacity;
         return colorObj + "";
       } else {
         throw colorStr + "is not a correct CSS color specifier";
       }
-      
     },
     renderColorRamp: function(opacity: number): void {
       const ctx = this.ctx;
@@ -154,25 +168,22 @@ export default Vue.extend({
       if (ctx != null) {
         d3.range(this.styleDiv.width).forEach(i => {
           let col;
-          switch(this.colorScaleType) {
-              case 'sequential':
-                  col = this.colorScale(this.legendScaleSequential.invert(i));
-                  break;
-              case 'ordinal':
-                  col = this.colorScale(this.legendScaleOrdinal(i));
+          switch (this.colorScaleType) {
+            case "sequential":
+              col = this.colorScale(this.legendScaleSequential.invert(i));
+              break;
+            case "ordinal":
+              col = this.colorScale(this.legendScaleOrdinal(i));
           }
 
-          ctx.fillStyle = this.addOpacityToColor(  
-            col,
-            opacity
-          );
-        
-        ctx.fillRect(i, 0, 1, this.canvasHeight);
+          ctx.fillStyle = this.addOpacityToColor(col, opacity);
+
+          ctx.fillRect(i, 0, 1, this.canvasHeight);
         });
       } else {
         throw "No canvas context found";
       }
     }
-  },
+  }
 });
 </script>
