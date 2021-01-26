65086f4c4349775650bd7ab7e3d3c175061f47d7
Nicolas Noé <nicolas.noe@inbo.be>
2021-01-26 10:51:29 +0100

    Made "language" string translatable (#104)

--- src/components/LanguageSelector.vue
+++ src/components/LanguageSelector.vue
@@ -1,17 +1,29 @@
 <template>
-  <b-nav-item-dropdown text="Language">
+  <b-nav-item-dropdown :text="t('Language')">
     <b-dropdown-item v-for="lang in availableLanguages" :key="lang.code" :active="lang.code === selectedLanguageCode" @click="selectedLanguageCode = lang.code">{{ lang.label }}</b-dropdown-item>
   </b-nav-item-dropdown>
 </template>
 
 <script lang="ts">
-import { LangCode, Language } from "@/CrowTypes";
+import { LangCode, Language, MultilanguageStringContainer } from "@/CrowTypes";
 import { ConfigStoreModule } from "@/store/ConfigStore";
 import { UserChoicesStoreModule } from "@/store/UserChoicesStore";
+import helpers from "@/helpers";
 import Vue from "vue";
 
 export default Vue.extend({
   name: "LanguageSelector",
+  data: function() {
+    return {
+      'texts': {
+        'Language': {
+          en: 'Language',
+          fr: 'Langue',
+          nl: 'Taal'
+        }
+      } as MultilanguageStringContainer
+    }
+  },
   computed: {
     availableLanguages(): Language[] {
       return ConfigStoreModule.availableLanguages;
@@ -24,6 +36,11 @@ export default Vue.extend({
         UserChoicesStoreModule.setSelectedLanguageCode(code);
       }
     }
+  },
+  methods: {
+    t(stringId: string) {
+      return helpers.translateString(stringId, this.selectedLanguageCode, this.texts);
+    },
   }
 });
 </script>
